10ab73c528905a18617223490b4ac9ab51ce6523
Abderrazaq MAKRAN <145991267+Amakran2003@users.noreply.github.com>
2025-03-12 09:45:38 +0100

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CraftBurgerCo/', // Utiliser un chemin relatif pour un déploiement flexible
+  base: './', // Utiliser un chemin relatif pour un déploiement flexible
   plugins: [react()],
   server: {
     // Improve mobile testing capabilities
